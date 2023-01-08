674f2694e3de65ecfe06e236f12668ccf2e978ad
Adrian Piątek <adr654231@gmail.com>
2023-01-08 20:19:52 +0100

    Rename directory pages to templates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { DefaultPageTemplate } from "pages/Default";
+import { DefaultPageTemplate } from "templates/Default";
 
 const DefaultPage = () => {
   return (
