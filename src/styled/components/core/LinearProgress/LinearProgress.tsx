df2b52a74260f112bbbc9298973803e049557af0
Adrian Piątek <adr654231@gmail.com>
2023-01-15 15:20:41 +0100

    Minor changes in linear progress component

--- src/styled/components/core/LinearProgress/LinearProgress.tsx
+++ src/styled/components/core/LinearProgress/LinearProgress.tsx
@@ -1,24 +1,36 @@
 import React from "react";
 
 import { LinearProgress as MuiLinearProgress } from "@mui/material";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { hexToRGBA } from "utils/colors";
 
-export const LinearProgress = () => {
-  return <LinearProgressElement />;
+type Position = "top" | "bottom";
+interface LinearProgressProps {
+  position?: Position;
+}
+
+export const LinearProgress = ({ position = "top" }: LinearProgressProps) => {
+  return <LinearProgressElement position={position} />;
 };
 
-const LinearProgressElement = styled(MuiLinearProgress)`
+const LinearProgressElement = styled(MuiLinearProgress)<LinearProgressProps>`
   &&& {
     position: absolute;
     top: 0px;
     left: 0px;
     right: 0px;
-    height: 4px;
+    height: 3px;
     background-color: ${({ theme }) => `${hexToRGBA(theme.primary05, 0.5)}`};
 
     & > span {
       background-color: ${({ theme }) => theme.primary05};
     }
+
+    ${({ position }) =>
+      position === "bottom" &&
+      css`
+        top: auto;
+        bottom: 0px;
+      `}
   }
 `;
