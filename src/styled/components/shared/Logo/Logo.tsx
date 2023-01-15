041829385c7c59a8980b206e2edeadd5d728bc0f
Adrian Piątek <adr654231@gmail.com>
2023-01-15 20:44:51 +0100

    Modify logo component

--- src/styled/components/shared/Logo/Logo.tsx
+++ src/styled/components/shared/Logo/Logo.tsx
@@ -1,22 +1,20 @@
-import React from "react";
+import React, { ComponentPropsWithoutRef } from "react";
 
 import TwitterIcon from "@mui/icons-material/Twitter";
 import styled from "styled-components";
 
 import { LogoColor, logoColors, LogoSize, logoSizeVariants } from "./logoStyleVariants";
 
-interface LogoProps {
+interface LogoProps extends ComponentPropsWithoutRef<typeof TwitterIcon> {
   color?: LogoColor;
   size?: LogoSize;
 }
 
-export const Logo = ({ size = "s", color = "primary" }: LogoProps) => {
-  return <LogoIcon size={size} color={color} />;
+export const Logo = ({ size = "s", color = "primary", ...props }: LogoProps) => {
+  return <LogoIcon {...props} size={size} color={color} />;
 };
 
 const LogoIcon = styled(TwitterIcon)<LogoProps>`
-  &&& {
-    ${({ color }) => logoColors[color ?? "primary"]}
-    ${({ size }) => logoSizeVariants[size ?? "s"]}
-  }
+  ${({ color }) => logoColors[color ?? "primary"]}
+  ${({ size }) => logoSizeVariants[size ?? "s"]}
 `;
