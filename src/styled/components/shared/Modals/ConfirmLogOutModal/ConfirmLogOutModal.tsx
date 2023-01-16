92cab43fe9028e68fa3e18fa21a00698ccf8fb09
Adrian Piątek <adr654231@gmail.com>
2023-01-16 21:26:30 +0100

    Add confirm log out modal component

---
+++ src/styled/components/shared/Modals/ConfirmLogOutModal/ConfirmLogOutModal.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { ConfirmModal } from "components/core";
+import { signOut } from "next-auth/react";
+
+interface ConfirmLogOutModalProps {
+  isOpen: boolean;
+  onAcceptSuccess?: () => void;
+  onClose: () => void;
+}
+
+export const ConfirmLogOutModal = ({
+  isOpen,
+  onAcceptSuccess,
+  onClose
+}: ConfirmLogOutModalProps) => {
+  const handleLogOut = () => {
+    signOut({
+      callbackUrl: "/home"
+    });
+
+    onAcceptSuccess?.();
+  };
+
+  return (
+    <ConfirmModal
+      isOpen={isOpen}
+      onAccept={handleLogOut}
+      onClose={onClose}
+      heading="Log out of Twitter?"
+      text="You can always log back in at any time. If you just want to switch accounts, you can do that by adding an existing account."
+      acceptButtonText="Log out"
+    />
+  );
+};
