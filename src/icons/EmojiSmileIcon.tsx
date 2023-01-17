09722a2a1788e4a35eef53bc5943dedbdb148b8f
Adrian Piątek <adr654231@gmail.com>
2023-01-17 22:49:53 +0100

    Add few icons

---
+++ src/icons/EmojiSmileIcon.tsx
@@ -0,0 +1,6 @@
+import React, { SVGProps } from "react";
+import { BsEmojiSmile } from "react-icons/bs";
+
+const EmojiSmileIcon = (props: SVGProps<SVGSVGElement>) => <BsEmojiSmile {...props} />;
+
+export default EmojiSmileIcon;
