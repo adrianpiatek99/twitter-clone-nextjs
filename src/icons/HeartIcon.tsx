bb785bf3f47c26e6229c0429d80d282225b2dba1
Adrian Piątek <adr654231@gmail.com>
2023-01-18 22:56:58 +0100

    Add few icons

---
+++ src/icons/HeartIcon.tsx
@@ -0,0 +1,6 @@
+import React, { SVGProps } from "react";
+import { AiOutlineHeart } from "react-icons/ai";
+
+const HeartIcon = (props: SVGProps<SVGSVGElement>) => <AiOutlineHeart {...props} />;
+
+export default HeartIcon;
