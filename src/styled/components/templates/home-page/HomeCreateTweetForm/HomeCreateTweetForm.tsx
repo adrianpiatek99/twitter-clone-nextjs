5c93b77c5309b4171f86cb03a1318ca8a0ca946a
Adrian Piątek <adr654231@gmail.com>
2023-01-18 15:49:08 +0100

    Add home create tweet form

---
+++ src/styled/components/templates/home-page/HomeCreateTweetForm/HomeCreateTweetForm.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useMutation } from "@tanstack/react-query";
+import { Textarea } from "components/core";
+import { useAppSession } from "hooks/useAppSession";
+import { useToasts } from "hooks/useToasts";
+import { createTweet } from "network/tweet/createTweet";
+import { tweetSchema, TweetValues } from "schema/tweetSchema";
+import { Avatar } from "shared/Avatar";
+import styled from "styled-components";
+
+import { HomeCreateTweetToolbar } from "./HomeCreateTweetToolbar";
+
+export const HomeCreateTweetForm = () => {
+  const { session, isUnauthenticated } = useAppSession();
+
+  if (isUnauthenticated || !session) return null;
+
+  const {
+    user: { screenName, profileImageUrl }
+  } = session;
+  const { register, handleSubmit, watch, reset } = useForm<TweetValues>({
+    resolver: yupResolver(tweetSchema),
+    defaultValues: {
+      text: ""
+    }
+  });
+  const { handleAddToast } = useToasts();
+  const tweetValue = watch("text");
+  const createTweetMutation = useMutation({
+    mutationFn: createTweet,
+    onSuccess: () => {
+      handleAddToast("success", "Tweet was created.");
+    },
+    onError: (error: any) => {
+      handleAddToast("error", error?.response?.message);
+    },
+    onSettled: () => {
+      reset();
+    }
+  });
+
+  const handleCreateTweet: SubmitHandler<TweetValues> = data => {
+    createTweetMutation.mutate({ ...data, imageUrls: [] });
+  };
+
+  return (
+    <Wrapper>
+      <LeftColumn>
+        <Avatar src={profileImageUrl} screenName={screenName} size="large" />
+      </LeftColumn>
+      <RightColumn>
+        <Textarea value={tweetValue} placeholder="What's happening?" {...register("text")} />
+        <HomeCreateTweetToolbar
+          tweetLength={tweetValue?.length ?? 0}
+          onSubmit={handleSubmit(handleCreateTweet)}
+          loading={createTweetMutation.isLoading}
+        />
+      </RightColumn>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  gap: 12px;
+  padding: 8px 16px;
+  border-top: 1px solid ${({ theme }) => theme.border};
+  border-bottom: 1px solid ${({ theme }) => theme.border};
+  margin-bottom: 4px;
+`;
+
+const LeftColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  width
+`;
+
+const RightColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  gap: 12px;
+`;
