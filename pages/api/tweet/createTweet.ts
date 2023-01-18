539771c33ded978c64d1a8b2cc36e8e3ce485d7f
Adrian Piątek <adr654231@gmail.com>
2023-01-18 14:27:23 +0100

    Add create tweet api

---
+++ pages/api/tweet/createTweet.ts
@@ -0,0 +1,47 @@
+import { Tweet } from "@prisma/client";
+import { NextApiHandler } from "next";
+import { getSession } from "next-auth/react";
+import { prisma } from "prisma/prisma";
+
+export type CreateTweetRequest = Pick<Tweet, "text" | "imageUrls">;
+export type CreateTweetResponse = Tweet | string;
+
+export const createTweetPath = "/api/tweet/createTweet";
+
+const handler: NextApiHandler<CreateTweetResponse> = async (req, res) => {
+  const session = await getSession({ req });
+  const { method } = req;
+  const body = req.body as CreateTweetRequest;
+
+  if (!session) {
+    return res.status(401).send("You are not authorized.");
+  }
+
+  if (method === "POST") {
+    const { text, imageUrls } = body;
+    const textTrim = text.trim();
+    const userId = session.user.id;
+
+    if (!textTrim || textTrim.length === 0) {
+      return res.status(403).send("Tweet text is required.");
+    }
+
+    const tweet = await prisma.tweet.create({
+      data: {
+        text: textTrim,
+        imageUrls,
+        author: {
+          connect: {
+            id: userId
+          }
+        }
+      }
+    });
+
+    res.status(201).json(tweet);
+  } else {
+    res.status(405);
+  }
+};
+
+export default handler;
