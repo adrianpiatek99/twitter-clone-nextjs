566f61d53991e755eaba1e8c50137e01e84bc1ce
Adrian Piątek <adr654231@gmail.com>
2023-01-21 11:42:38 +0100

    Add confirm delete tweet modal

---
+++ src/styled/components/shared/Modals/ConfirmDeleteTweetModal/ConfirmDeleteTweetModal.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { TweetData } from "api/tweet/timelineTweets";
+import { ConfirmModal } from "components/core";
+
+interface ConfirmDeleteTweetModalProps {
+  isOpen: boolean;
+  tweetId: TweetData["id"];
+  onAcceptSuccess: () => void;
+  onClose: () => void;
+}
+
+export const ConfirmDeleteTweetModal = ({
+  isOpen,
+  onAcceptSuccess,
+  onClose
+}: ConfirmDeleteTweetModalProps) => {
+  return (
+    <ConfirmModal
+      heading="Delete Tweet"
+      text="Are you sure you want to delete this Tweet?"
+      isOpen={isOpen}
+      onAccept={onAcceptSuccess}
+      onClose={onClose}
+      acceptButtonText="Delete"
+    />
+  );
+};
