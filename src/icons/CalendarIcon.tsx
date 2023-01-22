86485aa81d5c40bc6d9a5aa287bcf1d38a2dd68c
Adrian Piątek <adr654231@gmail.com>
2023-01-22 20:18:06 +0100

    Add few icons

---
+++ src/icons/CalendarIcon.tsx
@@ -0,0 +1,6 @@
+import React, { SVGProps } from "react";
+import { BiCalendar } from "react-icons/bi";
+
+const CalendarIcon = (props: SVGProps<SVGSVGElement>) => <BiCalendar {...props} />;
+
+export default CalendarIcon;
---
+++ src/icons/CameraPlusIcon.tsx
@@ -0,0 +1,6 @@
+import React, { SVGProps } from "react";
+import { TbCameraPlus } from "react-icons/tb";
+
+const CameraPlusIcon = (props: SVGProps<SVGSVGElement>) => <TbCameraPlus {...props} />;
+
+export default CameraPlusIcon;
