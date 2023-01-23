b5175efd73aee968f787454242d65ac2ee6d062c
Adrian Piątek <adr654231@gmail.com>
2023-01-23 11:56:02 +0100

    Modify useDeleteTweetMutation hook

--- src/hooks/useDeleteTweetMutation.ts
+++ src/hooks/useDeleteTweetMutation.ts
@@ -1,12 +1,14 @@
 import { QueryClient, useMutation } from "@tanstack/react-query";
-import { TimelineTweetsResponse, TweetData } from "api/tweet/timelineTweets";
+import { TimelineTweetsResponse } from "api/tweet/timelineTweets";
 import { deleteTweet } from "network/tweet/deleteTweet";
+import { reloadSession } from "utils/session";
 
 import { useToasts } from "./useToasts";
 
 interface UseDeleteTweetMutationProps {
   queryClient: QueryClient;
-  tweetId: TweetData["id"];
+  tweetId: string;
+  userId: string;
 }
 
 export interface UseDeleteTweetMutationReturn {
@@ -16,34 +18,41 @@ export interface UseDeleteTweetMutationReturn {
 
 export const useDeleteTweetMutation = ({
   queryClient,
-  tweetId
-}: UseDeleteTweetMutationProps): UseDeleteTweetMutationReturn => {
+  tweetId,
+  userId
+}: UseDeleteTweetMutationProps) => {
   const { handleAddToast } = useToasts();
   const deleteTweetMutation = useMutation({
     mutationFn: deleteTweet,
     onSuccess: () => {
-      queryClient.setQueryData<{ pages: TimelineTweetsResponse[] }>(
-        ["tweets", "infinite"],
-        oldData => {
-          if (oldData) {
-            const newTweets = oldData.pages.map(page => {
-              const tweets = page.tweets.filter(tweet => tweet.id !== tweetId);
+      // Remove the tweet from the cache on the home page
+      updateCache(["tweets", "infinite"]);
+      // Remove the tweet from the cache on the  profile page
+      updateCache(["tweets", "user", userId, "infinite"]);
 
-              return { ...page, tweets };
-            });
-
-            return { ...oldData, pages: newTweets };
-          }
-
-          return oldData;
-        }
-      );
+      reloadSession();
     },
     onError: (error: any) => {
       handleAddToast("error", error?.message);
     }
   });
   const deleteLoading = deleteTweetMutation.isLoading;
+
+  const updateCache = (queryKey: string[]) => {
+    queryClient.setQueryData<{ pages: TimelineTweetsResponse[] }>(queryKey, oldData => {
+      if (oldData) {
+        const newTweets = oldData.pages.map(page => {
+          const tweets = page.tweets.filter(tweet => tweet.id !== tweetId);
+
+          return { ...page, tweets };
+        });
+
+        return { ...oldData, pages: newTweets };
+      }
+
+      return oldData;
+    });
+  };
 
   const handleDeleteTweet = () => {
     if (deleteLoading) return;
