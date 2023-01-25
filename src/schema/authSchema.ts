33ed2a8777fca161b4ee7293c8921b78aa300287
Adrian Piątek <adr654231@gmail.com>
2023-01-25 16:06:16 +0100

    Modify auth schema

--- src/schema/authSchema.ts
+++ src/schema/authSchema.ts
@@ -2,8 +2,8 @@ import { containsEmojis } from "utils/strings";
 import * as yup from "yup";
 
 export const PROFILE_EMAIL_MAX_LENGTH = 25;
-export const PROFILE_SCREEN_NAME_MAX_LENGTH = 25;
-export const PROFILE_NAME_MAX_LENGTH = 16;
+export const PROFILE_SCREEN_NAME_MAX_LENGTH = 15;
+export const PROFILE_NAME_MAX_LENGTH = 50;
 export const PROFILE_PASSWORD_MAX_LENGTH = 16;
 
 export type SignInValues = {
@@ -44,19 +44,19 @@ export const signInSchema: yup.SchemaOf<SignInValues> = yup.object().shape({
 export const signUpSchema: yup.SchemaOf<SignUpValues> = yup.object().shape({
   screenName: yup
     .string()
-    .min(3, "Screen name must be at least 3 characters.")
+    .min(4, "Your username cannot be shorter than 4 characters.")
     .max(
       PROFILE_SCREEN_NAME_MAX_LENGTH,
-      `Screen name can't have more than ${PROFILE_SCREEN_NAME_MAX_LENGTH} characters.`
+      `Your username cannot be longer than ${PROFILE_SCREEN_NAME_MAX_LENGTH} characters.`
     )
     .required("Screen name is required.")
     .trim(),
   name: yup
     .string()
-    .min(3, "Name must be at least 3 characters.")
+    .min(4, "Your name cannot be shorter than 4 characters.")
     .max(
       PROFILE_NAME_MAX_LENGTH,
-      `Name can't have more than ${PROFILE_NAME_MAX_LENGTH} characters.`
+      `Your name cannot be longer than ${PROFILE_NAME_MAX_LENGTH} characters.`
     )
     .required("Name is required.")
     .trim(),
@@ -65,16 +65,16 @@ export const signUpSchema: yup.SchemaOf<SignUpValues> = yup.object().shape({
     .email("Must be a valid email.")
     .max(
       PROFILE_EMAIL_MAX_LENGTH,
-      `Email can't have more than ${PROFILE_EMAIL_MAX_LENGTH} characters.`
+      `Your email cannot be longer than ${PROFILE_EMAIL_MAX_LENGTH} characters.`
     )
     .required("Email is required.")
     .trim(),
   password: yup
     .string()
-    .min(4, "Password must be at least 4 characters.")
+    .min(4, "Your password cannot be shorter than 4 characters.")
     .max(
       PROFILE_PASSWORD_MAX_LENGTH,
-      `Password can't have more than ${PROFILE_PASSWORD_MAX_LENGTH} characters.`
+      `Your password cannot be longer than ${PROFILE_PASSWORD_MAX_LENGTH} characters.`
     )
     .test("emoji", "⛔ No entry emojis", value => !containsEmojis(value))
     .required("Password is required."),
