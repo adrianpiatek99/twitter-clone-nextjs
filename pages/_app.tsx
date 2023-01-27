57bbabf6c3735c6804575fac62d1560f0518a2ae
Adrian Piątek <adr654231@gmail.com>
2023-01-27 21:38:56 +0100

    Add useScrollRestoration hook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useScrollRestoration } from "hooks/useScrollRestoration";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import ReduxStoreProvider from "store/ReduxStoreProvider";
 import { GlobalStyle, ThemeProvider } from "styled/theme";
 import Layout from "templates/layout";
 
-const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
+const App = ({ Component, pageProps: { session, ...pageProps }, router }: AppProps) => {
   const [queryClient] = useState(() => new QueryClient());
+
+  useScrollRestoration(router);
 
   return (
     <QueryClientProvider client={queryClient}>
