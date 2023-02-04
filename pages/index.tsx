fcdc065de19bd2363cbc95029dfdad87fa25ef83
Adrian Piątek <adr654231@gmail.com>
2023-02-04 13:22:11 +0100

    Redirect from login page to home if you are logged in

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
+import LoginContextProvider from "context/LoginContext";
+import type { GetSessionParams } from "next-auth/react";
+import { getSession } from "next-auth/react";
 import { LoginPage } from "templates/login-page";
 
 const Login = () => {
   return (
     <>
-      <LoginPage />
+      <LoginContextProvider>
+        <LoginPage />
+      </LoginContextProvider>
     </>
   );
 };
+
+export async function getServerSideProps(context: GetSessionParams) {
+  const session = await getSession(context);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/home",
+        permanent: false
+      }
+    };
+  }
+
+  return {
+    props: {}
+  };
+}
 
 export default Login;
