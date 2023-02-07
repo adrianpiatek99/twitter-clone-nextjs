3147a92c63a9016f6e90062d2d8c3c0d68b18e46
Adrian Piątek <adr654231@gmail.com>
2023-02-07 12:48:32 +0100

    Add retweet icon

---
+++ src/icons/RetweetIcon.tsx
@@ -0,0 +1,7 @@
+import type { SVGProps } from "react";
+import React from "react";
+import { AiOutlineRetweet } from "react-icons/ai";
+
+const RetweetIcon = (props: SVGProps<SVGSVGElement>) => <AiOutlineRetweet {...props} />;
+
+export default RetweetIcon;
