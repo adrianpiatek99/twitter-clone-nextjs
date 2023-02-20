05dd898872f3b8013a460e15c13d2f8285e631a1
Adrian Piątek <adr654231@gmail.com>
2023-02-20 10:14:02 +0100

    Import LoginPage as lazy (#61)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,21 @@
-import { LoginPage } from "components/login-page";
+import { Loader } from "components/core";
 import LoginContextProvider from "context/LoginContext";
+import dynamic from "next/dynamic";
 import type { GetSessionParams } from "next-auth/react";
 import { getSession } from "next-auth/react";
 import { NextSeo } from "next-seo";
+import styled from "styled-components";
+
+const LazyLoginPage = dynamic(
+  () => import("../src/styled/components/login-page/LoginPage").then(mod => mod.LoginPage),
+  {
+    loading: () => (
+      <LazyLoaderContainer>
+        <Loader />
+      </LazyLoaderContainer>
+    )
+  }
+);
 
 const Login = () => {
   return (
@@ -12,7 +25,7 @@ const Login = () => {
         description="Log in to Twitter to see the latest. Join the conversation, follow accounts, see your Home Timeline, and catch up on Tweets from the people you know."
       />
       <LoginContextProvider>
-        <LoginPage />
+        <LazyLoginPage />
       </LoginContextProvider>
     </>
   );
@@ -36,3 +49,9 @@ export async function getServerSideProps(context: GetSessionParams) {
 }
 
 export default Login;
+
+const LazyLoaderContainer = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+`;
